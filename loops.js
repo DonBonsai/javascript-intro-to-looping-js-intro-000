cb8e63d8da76db3924bc74381c7e02c6b132c6ed
Donelle Reed <donellereed@gmail.com>
2019-06-30 23:57:27 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
-var function forLoop(arr){
-  for (var i = 0; i =< 25 ; i++) {
+function forLoop(arr){
+  for (var i = 0; i < 25 ; i++) {
     if (i === 1 ){
       arr.push("I am 1 strange loop.")
     } else {
